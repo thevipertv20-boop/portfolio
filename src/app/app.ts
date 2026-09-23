da3ab95e61a6002d497a7aa1fcc31aa38695cf00
Sebastian <thevipertv20@gmail.com>
2026-09-23 10:03:29 +0200

    feat: update about me and navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
+import { AboutMe } from './components/about-me/about-me';
 
 @Component({
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, AboutMe],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
---
+++ src/app/components/about-me/about-me.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-about-me',
+  styleUrl: './about-me.scss',
+  templateUrl: './about-me.html',
+})
+export class AboutMe {}
